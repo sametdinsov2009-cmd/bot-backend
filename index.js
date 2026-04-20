de5945a2e92b1239560f38692140a48f8a842e51
sametdinsov2009-cmd <sametdinsov2009@gmail.com>
2026-04-20 23:58:39 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/api/chat', async (req, res) => {
 
         // 3. Запускаем Gemini с историей
         const model = genAI.getGenerativeModel({ 
-            model: "gemini-3.1-flash-lite",
+            model: "gemini-3.1-flash-lite-preview",
             systemInstruction: SYSTEM_INSTRUCTION
         });
         
